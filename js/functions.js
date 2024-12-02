0d82fd28d4046d1191a2a440e4a420501845a881
alisameta <alisameta09@gmail.com>
2024-12-02 17:06:35 +0300

    Change in function isPalindrome

--- js/functions.js
+++ js/functions.js
@@ -2,28 +2,30 @@ const checkStringLength = (string, maxlength) => string.length <= maxlength;
 checkStringLength('проверяемая строка', 20);
 
 
-const isPalindrome = (string) => string.replaceAll(' ', '').toLowerCase();
-let reversedString = '';
+const isPalindrome = (string) => {
+  string = string.replaceAll(' ', '').toLowerCase();
+  let reversedString = '';
 
-for (let i = string.length - 1; i >= 0; i--) {
-  reversedString += string[i];
-  
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
   return reversedString === string;
 };
+
 isPalindrome('топот');
 
 
 const exctractNumbers = (string) => {
-string = String(string);
-let result = '';
+  string = String(string);
+  let result = '';
 
-for (let i = 0; i < string.length; i++) {
-if (Number.isNaN(parseInt(string[i], 10))) {
-  result += string[i];
-}
-}
-return parseInt(result, 10);
+  for (let i = 0; i < string.length; i++) {
+    if (Number.isNaN(parseInt(string[i], 10))) {
+      result += string[i];
+    }
+  }
+  return parseInt(result, 10);
 };
 
 exctractNumbers ('2023 Год');
-  
+
